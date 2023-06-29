4575ee91e4cd1370f2c16b25a374dd6cb33b8d02
Travis LaDuke <travis@tl-air.local>
2023-06-29 10:48:09 -0700

    hi

--- flake.nix
+++ flake.nix
@@ -1,22 +1,85 @@
 {
-  description = "My first nix flake";
+  description = "travis' mac computers";
 
   inputs = {
-      nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-22.05-darwin";
-      home-manager.url = "github:nix-community/home-manager";
-      home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    # nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-23.05-darwin";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
-      # nix will normally use the nixpkgs defined in home-managers inputs, we only want one copy of nixpkgs though
-      darwin.url = "github:lnl7/nix-darwin";
-      darwin.inputs.nixpkgs.follows = "nixpkgs"; # ...
+    darwin.url = "github:lnl7/nix-darwin";
+    darwin.inputs.nixpkgs.follows = "nixpkgs";
+
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+
+    rust-overlay.url = "github:oxalica/rust-overlay";
+
   };
 
-  outputs = { self, nixpkgs, home-manager, darwin }: {
-
+  outputs = { self, nixpkgs, home-manager, darwin, rust-overlay }: {
     darwinConfigurations.cattail = darwin.lib.darwinSystem {
+      system = "aarch64-darwin";
+      modules = [
+        ./hosts/cattail/default.nix
 
-    system = "aarch64-darwin";
-    modules = [ ./hosts/cattail/default.nix ];
+        ({ pkgs, ... }: {
+          nixpkgs.overlays = [ rust-overlay.overlays.default ];
+          environment.systemPackages = [
+            # pkgs.rust-bin.stable.latest.default
+            (pkgs.rust-bin.stable.latest.default.override {
+              targets = [ "x86_64-apple-darwin" ];
+            })
+          ];
+        })
+
+        home-manager.darwinModules.home-manager {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.users.travis = { pkgs, ... }: {
+            home.stateVersion = "23.11";
+            home.packages = [
+              pkgs.tmux
+              pkgs.ipcalc
+              pkgs.htop
+              pkgs.git
+              pkgs.ripgrep
+              pkgs.fd
+              pkgs.jq
+              pkgs.nodejs_18
+              pkgs.go
+            ];
+
+            home.sessionPath = [
+              "$HOME/.config/emacs/bin"
+              "$HOME/go/bin"
+            ];
+
+            home.sessionVariables = {
+              EDITOR = "vim";
+            };
+
+            programs.git = {
+              enable = true;
+              userName = "travisladuke";
+              userEmail = "travisladuke@gmail.com";
+            };
+
+            programs.zsh  = {
+              enable = true;
+              shellAliases = {
+                ll = "ls -l";
+              };
+
+              initExtra = ''
+                if [[ $(uname -m) == 'arm64' ]]; then
+                  eval "$(/opt/homebrew/bin/brew shellenv)"
+                    fi
+              '';
+
+            };
+
+          };
+        }
+      ];
     };
   };
 }
--- hosts/cattail/default.nix
+++ hosts/cattail/default.nix
@@ -1,30 +1,45 @@
-{ pkgs, ... }:
+{ pkgs, config, lib, ... }:
 {
 
   # Make sure the nix daemon always runs
   services.nix-daemon.enable = true;
 
-  # Installs a version of nix, that dosen't need "experimental-features = nix-command flakes" in /etc/nix/nix.conf
-  services.nix-daemon.package = pkgs.nixFlakes;
-
   # if you use zsh (the default on new macOS installations),
   # you'll need to enable this so nix-darwin creates a zshrc sourcing needed environment changes
-  programs.zsh.enable = true;
+  # needed here and in home-manager
+  programs.zsh  = {
+    enable = true;
+  };
   # bash is enabled by default
+
+  users.users.travis.home = "/Users/travis";
+
+  homebrew = {
+    enable = true;
+    onActivation.autoUpdate = true;
+    # updates homebrew packages on activation,
+    # can make darwin-rebuild much slower (otherwise i'd forget to do it ever though)
+    casks = [
+      # "rectangle" spectacle fork
+      "amethyst" # tiling
+      "discord"
+      "zoom"
+      "signal"
+      "steam"
+    ];
+    taps = [ "d12frosted/emacs-plus" ];
+
+    brews = [
+      "gh"
+      {
+        name = "emacs-plus@28";
+        args = [ "with-native-comp" "with-imagemagick" "with-modern-doom3-icon" ];
+        restart_service = "changed";
+      }
+
+    ];
+  };
+
 
 }
 
-homebrew = {
-  enable = true;
-  autoUpdate = true;
-  # updates homebrew packages on activation,
-  # can make darwin-rebuild much slower (otherwise i'd forget to do it ever though)
-  casks = [
-    # "hammerspoon"
-    # "amethyst"
-    # "alfred"
-    # "logseq"
-    # "discord"
-    # "iina"
-  ];
-};
